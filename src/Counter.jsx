a0ad7018889ef9954b07e9aa7fc8e463860fc0d6
mayurmundankar <mundankarmayur0663@gmail.com>
2024-12-31 19:07:08 +0530

    counter-app

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+    const reset = () => {
+        setCount(0);
+    }
+    
+
+    return(
+        <div className='container'>
+            <p className='paragraph'>{count}</p>
+            <button className='btn' onClick={increment}>Increment</button>
+            <button className='btn' onClick={decrement}>Decrement</button>
+            <button className='btn' onClick={reset}>Reset</button>
+        </div>
+    )
+}
+
+export default Counter;
